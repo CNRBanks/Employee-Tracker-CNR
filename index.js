0717ab0f3ccd4b94d4e8e81fd30f0e7d7d9fa55a
unknown <connortamminen@gmail.com>
2022-12-04 12:38:40 -0600

    redoing assignment and following tutorial

---
+++ index.js
@@ -0,0 +1,7 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+// add departments, roles, and employees
+// view departments, roles, and employees
+// update employee roles
+
